c5f4c069fc941fc307720035cb33c2144ffaf3a8
Vagner Zampieri <vagner.zampieri@myfreecomm.com.br>
2018-09-25 19:41:56 -0300

    Add form to receivable

--- app/src/main/java/com/example/scott_pilgrim/financask/ui/activity/ListaTransacoesActivity.kt
+++ app/src/main/java/com/example/scott_pilgrim/financask/ui/activity/ListaTransacoesActivity.kt
@@ -1,14 +1,24 @@
 package com.example.scott_pilgrim.financask.ui.activity
 
+import android.app.DatePickerDialog
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.example.scott_pilgrim.financask.R
+import com.example.scott_pilgrim.financask.extension.dataFormatada
 import com.example.scott_pilgrim.financask.model.Tipo
 import com.example.scott_pilgrim.financask.model.Transacao
 import com.example.scott_pilgrim.financask.ui.ResumoView
 import com.example.scott_pilgrim.financask.ui.adapter.ListaTransacoesAdapter
 import kotlinx.android.synthetic.main.activity_lista_transacoes.*
+import kotlinx.android.synthetic.main.form_transacao.view.*
 import java.math.BigDecimal
+import java.util.*
 
 class ListaTransacoesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,8 +28,44 @@ class ListaTransacoesActivity : AppCompatActivity() {
         val transacoes = transacoesExemplos()
 
         configurarResumo(transacoes)
+        configurarLista(transacoes)
 
-        lista_transacoes_listview.adapter = configuraTransacoes(transacoes)
+        lista_transacoes_adiciona_receita.setOnClickListener {
+            val view : View = window.decorView
+            val viewCriada = LayoutInflater.from(this)
+                    .inflate(R.layout.form_transacao, view as ViewGroup, false)
+
+            val year = 2018
+            val month = 8
+            val dayOfMonth = 25
+
+            val today = Calendar.getInstance()
+
+            viewCriada.form_transacao_data.setOnClickListener {
+                DatePickerDialog(this,
+                        DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+                            val dataSelecionada = Calendar.getInstance()
+                            dataSelecionada.set(year, month, dayOfMonth)
+                            viewCriada.form_transacao_data
+                                .setText(dataSelecionada.dataFormatada())
+                        }, year, month, dayOfMonth)
+                .show()
+            }
+
+            val adapter = ArrayAdapter
+                    .createFromResource(this,
+                            R.array.categorias_de_receita,
+                            android.R.layout.simple_spinner_dropdown_item)
+
+            viewCriada.form_transacao_categoria.adapter = adapter
+
+            AlertDialog.Builder(this)
+                    .setTitle(R.string.adiciona_receita)
+                    .setView(viewCriada)
+                    .setPositiveButton("Adicionar", null)
+                    .setNegativeButton("Cancelar", null)
+                    .show()
+        }
     }
 
     fun configurarResumo(transacoes: List<Transacao>) {
@@ -28,8 +74,10 @@ class ListaTransacoesActivity : AppCompatActivity() {
         resumoView.atualiza()
     }
 
-    private fun configuraTransacoes(transacoes: List<Transacao>) =
-            ListaTransacoesAdapter(transacoes, this)
+    private fun configurarLista(transacoes: List<Transacao>) {
+        lista_transacoes_listview.adapter = ListaTransacoesAdapter(transacoes, this)
+    }
+
 
     private fun transacoesExemplos(): List<Transacao> {
         return listOf(
@@ -54,3 +102,4 @@ class ListaTransacoesActivity : AppCompatActivity() {
         )
     }
 }
+
